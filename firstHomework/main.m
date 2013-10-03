ffbff4f64eeaf36e9df9b9351abae82b0f1480a8
7Charlotte <liuxia@liuxiatekiMacBook-Air.local>
2013-10-03 13:47:40 +0800

    Initial Commit

---
+++ firstHomework/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  firstHomework
+//
+//  Created by Charlotte on 13年10月3日.
+//  Copyright (c) 2013年 Charlotte. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
